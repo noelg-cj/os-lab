286d57c47d8e4514ad6541b29b375b7b2d76e033
= <noelgeorge2004@gmail.com>
2024-04-02 02:08:29 +0530

    palindrome

---
+++ q210.sh
@@ -0,0 +1,16 @@
+function contains_only_123 {
+    local number=$1
+    local pattern="^[123]+$"
+    if [[ $number =~ $pattern ]]; then
+        return 0 # Number contains only 1, 2, and 3
+    else
+        return 1 # Number contains other digits
+    fi
+}
+
+# Loop through all 3-digit numbers
+for (( i=100; i<=999; i++ )); do
+    if contains_only_123 "$i"; then
+        echo $i
+    fi
+done
